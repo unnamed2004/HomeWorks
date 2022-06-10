7c0cd99c6e2e8d2fbf05224a9bea59ed73aa9e6d
umar gafurov <rokarokaev@gmail.com>
2022-06-10 20:25:34 +0430

    task4

--- hometask/task4.js
+++ hometask/task4.js
@@ -1,7 +1,3 @@
 let L = +prompt("Enter L :")
-let R = +prompt("Enter R2 :")
-let S = +prompt("Enter S1 :")
-L = 2 * Math.PI * R
-S = Math.PI * R**2
-alert(`L = ${L}
-      S = ${S}`)
+R= L /(2*Math.PI)
+alert(`R = ${R} `)
