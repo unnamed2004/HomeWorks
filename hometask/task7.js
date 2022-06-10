ee2f3c211339b8137ab75cf206157d3f8d49d974
umar gafurov <rokarokaev@gmail.com>
2022-06-11 00:38:11 +0430

    task 7 8

---
+++ hometask/task7.js
@@ -0,0 +1,12 @@
+let A = +prompt("Enter A :")
+let B = +prompt("Enter B :")
+let C = +prompt("Enter C :")
+A =Math.abs(C-A);
+B =Math.abs(C-B);
+C =Math.abs(C-A) + Math.abs(C-B)
+alert(`A = ${A} \n
+      B = ${B} \n
+      C = ${C}`)
+
+
+
---
+++ hometask/task8.js
@@ -0,0 +1,7 @@
+let x1 = +prompt("Enter x1 :")
+let x2 = +prompt("Enter x2 :")
+let y1 = +prompt("Enter y1 :")
+let y2 = +prompt("Enter y2 :")
+
+c=Math.sqrt (x2 - x1)**2 + (y2 - y1)**2
+alert(`c = ${c}`)
