95ab2eb43313b5fcdd7a50f7781bdff09086dc15
umar gafurov <rokarokaev@gmail.com>
2022-06-13 10:39:49 +0430

    metod

---
+++ metod/medod.js
@@ -0,0 +1,23 @@
+//                         trim
+// // var orig = 'foo    ';
+// // console.log(orig.trim()); // 'foo'
+
+
+//                             charAt
+// // var anyString = 'Дивный новый мир';
+// // console.log(" " + anyString.charAt(0)   + "");
+
+
+//                             includes
+// var str = 'Быть или не быть вот в чём вопрос.';
+// console.log(str.includes('Быть'));       // true
+
+
+//                            lastIndexOf
+// var anyString = 'Дивный новый мир';
+// console.log( + anyString.lastIndexOf('й'));
+
+
+//                        IndexOf
+// // var anyString = 'Дивный новый мир';
+// // console.log( + anyString.IndexOf('й'));
