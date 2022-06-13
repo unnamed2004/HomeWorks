51b67739d6128675dde6b09acd395db925737f8c
umar gafurov <rokarokaev@gmail.com>
2022-06-13 21:54:30 +0430

    case

---
+++ hometask/home2/case2.js
@@ -0,0 +1,22 @@
+
+let ned = prompt("Enter ned")
+switch (ned)
+{
+case '1':
+console.log('Bad');
+break;
+case '2':
+console.log("unsatisfactory");
+break;
+case '3':
+console.log('satisfactorily');
+break;
+case '4':
+console.log('Good');
+break;
+case '5':
+console.log('Great');
+break;
+default:
+    console.log('hmmmm');
+}
