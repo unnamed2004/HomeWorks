3a76e818fc45072cf9c0090540e4b4483ce0bd60
umar gafurov <rokarokaev@gmail.com>
2022-06-13 22:11:20 +0430

    case

---
+++ hometask/home2/case3.js
@@ -0,0 +1,42 @@
+let ned = prompt("Enter ned")
+switch (ned)
+{
+case '1':
+console.log('Январь  зима');
+break;
+case '2':
+console.log("февраль зима");
+break;
+case '3':
+console.log('март весна');
+break;
+case '4':
+console.log('апрель весна');
+break;
+case '5':
+console.log('май весна');
+break;
+case '6':
+console.log('июнь лето');
+break;
+case '7':
+console.log('июль лето');
+break;
+case '8':
+console.log('август лето');
+break;
+case '9':
+console.log('сентябрь осень');
+break;
+case '10':
+console.log('октябрь осень');
+break;
+case '11':
+console.log('ноябрь осень');
+break;
+case '12':
+console.log('декабрь зима');
+break;
+default:
+    console.log('i dont кnow');
+}
---
+++ hometask/home2/case4.js
@@ -0,0 +1,42 @@
+let ned = prompt("Enter ned")
+switch (ned)
+{
+case '1':
+console.log('Январь  31');
+break;
+case '2':
+console.log("февраль 28");
+break;
+case '3':
+console.log('март 31');
+break;
+case '4':
+console.log('апрель 30');
+break;
+case '5':
+console.log('май 31');
+break;
+case '6':
+console.log('июнь 30');
+break;
+case '7':
+console.log('июль 31');
+break;
+case '8':
+console.log('август 31');
+break;
+case '9':
+console.log('сентябрь 30');
+break;
+case '10':
+console.log('октябрь 31');
+break;
+case '11':
+console.log('ноябрь 30');
+break;
+case '12':
+console.log('декабрь 31');
+break;
+default:
+    console.log('365');
+}
