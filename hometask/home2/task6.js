07a547dd03144aeb414f15d1c7ea54287318faaa
umar gafurov <rokarokaev@gmail.com>
2022-06-13 20:17:57 +0430

    task6

---
+++ hometask/home2/task6.js
@@ -0,0 +1,17 @@
+let x =+prompt("Enter x")
+if (x==0){
+console.log("nul cheslo")
+}
+else if (x>0){
+    console.log("Polozhitel");
+}
+else{
+    console.log("Otritzatel");
+}
+
+if (x%2==0){
+    console.log("chotniy");
+}
+else{
+    console.log("nechot");
+}
