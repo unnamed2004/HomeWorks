4a56c071ce34bb5112aeb701afcd037ec882a9a7
umar gafurov <rokarokaev@gmail.com>
2022-06-14 09:22:47 +0430

    for

--- hometask/home2/for2.js
+++ hometask/home2/for2.js
@@ -1,6 +1,6 @@
 let k =+prompt("Enter k")
 let n =+prompt("Enter n")
-for (let i=0;i<k;i++){
+for (let i=k;i<=n;i++){
 
-    console.log(i);
+     console.log(i);
 }
---
+++ hometask/home2/for3.js
@@ -0,0 +1,5 @@
+let a =+prompt("Enter a")
+let b =+prompt("Enter b")
+for (let i=b;i>=a;i--){
+    console.log(i);
+}   
